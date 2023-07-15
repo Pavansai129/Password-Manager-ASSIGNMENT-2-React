83279ab2522b601f28b177ee0704a514ee5ff639
pavansai <pavansaivvit@gmail.com>
2023-07-15 07:32:06 +0000

    have to write code for passwords card

--- src/components/PasswordManager/index.js
+++ src/components/PasswordManager/index.js
@@ -12,56 +12,68 @@ class PasswordManager extends Component {
     const {userName, userPassword, passwordsList} = this.state
     return (
       <div className="app-container">
-        <div className="password-manager-logo-container">
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/password-manager-logo-img.png"
-            alt="app logo"
-            className="app-logo"
-          />
-        </div>
-        <div className="form-input-and-password-manager-image-container">
-          <div className="user-name-password-input-container">
-            <form onSubmit={this.addPassword}>
-              <h1>Add New Password</h1>
-              <div className="website-input-container">
-                <img
-                  src="https://assets.ccbp.in/frontend/react-js/password-manager-website-img.png"
-                  alt="username"
-                  className="website-logo"
-                />
-                <input
-                  placeholder="Enter Website"
-                  type="text"
-                  className="website-input"
-                />
-              </div>
-              <div>
-                <img
-                  src="https://assets.ccbp.in/frontend/react-js/password-manager-username-img.png"
-                  alt="username"
-                />
-                <input placeholder="Enter Username" type="text" />
-              </div>
-              <div>
-                <img
-                  src="https://assets.ccbp.in/frontend/react-js/password-manager-password-img.png"
-                  alt="username"
-                />
-                <input placeholder="Enter Password" type="text" />
-              </div>
-              <div className="add-button-container">
-                <button type="submit" className="button">
-                  Add
-                </button>
-              </div>
-            </form>
-          </div>
-          <div>
+        <div className="password-manager-container">
+          <div className="password-manager-logo-container">
             <img
-              src="https://assets.ccbp.in/frontend/react-js/password-manager-lg-img.png"
-              alt="password manager"
-              className="password-manager-image"
+              src="https://assets.ccbp.in/frontend/react-js/password-manager-logo-img.png"
+              alt="app logo"
+              className="app-logo"
             />
+          </div>
+          <div className="form-input-and-password-manager-image-container">
+            <div className="user-name-password-input-container">
+              <form onSubmit={this.addPassword}>
+                <h1 className="add-new-password-text">Add New Password</h1>
+                <div className="website-input-container">
+                  <img
+                    src="https://assets.ccbp.in/frontend/react-js/password-manager-website-img.png"
+                    alt="username"
+                    className="website-logo"
+                  />
+                  <input
+                    placeholder="Enter Website"
+                    type="text"
+                    className="website-input"
+                  />
+                </div>
+                <div className="username-input-container">
+                  <img
+                    src="https://assets.ccbp.in/frontend/react-js/password-manager-username-img.png"
+                    alt="username"
+                    className="user-logo"
+                  />
+                  <input
+                    placeholder="Enter Username"
+                    type="text"
+                    className="username-input"
+                  />
+                </div>
+                <div className="password-input-container">
+                  <img
+                    src="https://assets.ccbp.in/frontend/react-js/password-manager-password-img.png"
+                    alt="username"
+                    className="password-logo"
+                  />
+                  <input
+                    placeholder="Enter Password"
+                    type="text"
+                    className="password-input"
+                  />
+                </div>
+                <div className="add-button-container">
+                  <button type="submit" className="add-button">
+                    Add
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div className="password-manager-image-container">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/password-manager-lg-img.png"
+                alt="password manager"
+                className="password-manager-image"
+              />
+            </div>
           </div>
         </div>
       </div>
